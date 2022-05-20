e4a6cd8fa6e1a76bfbad0988b13b7ab554af879d
Subhradip Sarker <ss3955@cornell.edu>
2022-05-20 16:26:43 -0400

    Books Create/Indiex/new

--- routes/books.js
+++ routes/books.js
@@ -10,14 +10,34 @@ const multer = require('multer')
 const imageMimeTypes = ['images/jpeg','images/png','images/gif']
 const upload = multer({
     dest: uploadPath,
-    // fileFilter: (req, file, callback) ={
-    //     callback(null, )
-    // }
+    fileFilter: (req, file, callback) => {
+        callback(null, imageMimeTypes.includes(file.mimetype))
+    }
 })
 
 //All Books Route
 router.get('/', async (req,res)=>{
-    res.send('All books')
+    //res.send('All books')
+    let query = Book.find()
+    if (req.query.title != null && req.query.title != '') {
+        query = query.regex('title', new RegExp(req.query.title, 'i'))
+    }
+    if (req.query.publishedBefore != null && req.query.publishedBefore != '') {
+        query = query.lte('publishDate', req.query.publishedBefore)
+    }
+    if (req.query.publishedAfter != null && req.query.publishedAfter != '') {
+        query = query.gte('publishDate', req.query.publishedAfter)
+    }
+    try {
+        const books = await query.exec()
+        res.render('books/index', {
+            books:books,
+            searchOptions: req.query
+        })
+    } catch {
+        res.redirect('/')
+    }
+
 
 })
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const Book = require('../models/book')
 
-router.get('/', (req,res)=>{
+router.get('/', async (req,res)=>{
     //res.send('Okay server working')
-    res.render('index')
+    let books 
+    try {
+        books = await Book.find().sort({ createAt: 'desc'}).limit(10).exec()
+    } catch {
+        books = []
+
+    }
+    res.render('index', {books: books})
 })
 
 module.exports = router
